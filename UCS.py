e4b96eae7b2fdebbe0df00ffc21608a124151699
nicolas_alahmar <nicolasalahmar@gmail.com>
2022-11-21 20:29:26 +0300

    ucs is now printing the path

--- UCS.py
+++ UCS.py
@@ -6,19 +6,21 @@ class UCS:
     MAX_INT = 999999
 
     def __init__(self, board):
-        self.dist = [[UCS.MAX_INT for _ in range(board.Dimensions[1] + 1)] for _ in range(board.Dimensions[0] + 1)]   # dist list
-        # self.visited
+        self.dist = [[UCS.MAX_INT for _ in range(board.Dimensions[1] + 1)] for _ in
+                     range(board.Dimensions[0] + 1)]  # dist list
         self.q = PriorityQueue()  # priority queue for ucs implementation
         self.board = board
+        self.w = 1  # edge cost is always 1
+        self.parent = {}
 
     def ucs(self):
         x, y = self.board.CastlePosition
         self.dist[x][y] = 0
         self.q.push((self.board, 0))  # mark it as visited
+        self.parent[(x, y)] = (-1, -1)
 
         while not self.q.isEmpty():
             current_state, path = self.q.pop()  # pop the node we want to process from the queue
-            current_state.path.append(current_state)
 
             x, y = current_state.CastlePosition
 
@@ -26,10 +28,10 @@ class UCS:
                 continue
 
             for child in current_state.get_next_states():
-                w = 1   # edge cost is always 1
-                x, y = child.CastlePosition     # x and y of child
-                if path + w < self.dist[x][y]:
-                    self.dist[x][y] = path + w
+                x, y = child.CastlePosition  # x and y of child
+                if path + self.w < self.dist[x][y]:
+                    self.parent[(x, y)] = current_state.CastlePosition
+                    self.dist[x][y] = path + self.w
                     self.q.push((child, self.dist[x][y]))
 
     def loop(self):
@@ -37,5 +39,12 @@ class UCS:
         self.ucs()
         t2 = time.perf_counter()
         print(self.dist[10][10])
-        # print("number of visited nodes", s)
+        print("the path:")
+
+        i = (10, 10)
+        while i != (-1, -1):
+            print(i, end=' ')
+            i = self.parent[i]
+
+        print()
         print("time elapsed:", round(t2 - t1, 4), "s")

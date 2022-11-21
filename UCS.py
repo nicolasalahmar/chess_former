e3f4c9bff7de9040bcc10a1ce905514c19fe275a
nicolas_alahmar <nicolasalahmar@gmail.com>
2022-11-21 22:15:44 +0300

    ucs stops when it gets to the kingposition printing is in the right order with a recursive function

--- UCS.py
+++ UCS.py
@@ -2,6 +2,14 @@ import time
 from PriorityQueue import PriorityQueue
 
 
+def initialize_dict(board):
+    temp = {}
+    for i in range(board.Dimensions[0] + 1):
+        for j in range(board.Dimensions[1] + 1):
+            temp[(i, j)] = []
+    return temp
+
+
 class UCS:
     MAX_INT = 999999
 
@@ -11,19 +19,21 @@ class UCS:
         self.q = PriorityQueue()  # priority queue for ucs implementation
         self.board = board
         self.w = 1  # edge cost is always 1
-        self.parent = {}
+        self.parent = initialize_dict(self.board)
         self.n = 0
 
     def ucs(self):
         x, y = self.board.CastlePosition
         self.dist[x][y] = 0
         self.q.push((self.board, 0))  # mark it as visited
-        self.parent[(x, y)] = (-1, -1)
 
         while not self.q.isEmpty():
             current_state, path = self.q.pop()  # pop the node we want to process from the queue
 
             x, y = current_state.CastlePosition
+
+            if current_state.Solved():
+                return current_state
 
             if path > self.dist[x][y]:
                 continue
@@ -32,22 +42,27 @@ class UCS:
                 self.n += 1
                 x, y = child.CastlePosition  # x and y of child
                 if path + self.w < self.dist[x][y]:
-                    self.parent[(x, y)] = current_state.CastlePosition
+                    self.parent[(x, y)].append(current_state)
                     self.dist[x][y] = path + self.w
                     self.q.push((child, self.dist[x][y]))
 
+    def print_solution(self, i):
+        if not self.parent[i.CastlePosition]:
+            return
+        self.print_solution(self.parent[i.CastlePosition].pop())
+        print("=====================================================")
+        i.print()
+
     def loop(self):
         t1 = time.perf_counter()
-        self.ucs()
+        solution = self.ucs()
         t2 = time.perf_counter()
-        print(self.dist[10][10])
         print("the path:")
 
-        i = (10, 10)
-        while i != (-1, -1):
-            print(i, end=' ')
-            i = self.parent[i]
+        self.print_solution(solution)
 
         print()
         print("the number of nodes is:", self.n)
+        print("the perfect amount of moves to get to the answer:",
+              self.dist[self.board.KingPosition[0]][self.board.KingPosition[1]])
         print("time elapsed:", round(t2 - t1, 4), "s")

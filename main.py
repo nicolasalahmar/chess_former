1865ea5709efdcfe51c292143d997f6df1459942
nicolas_alahmar <nicolasalahmar@gmail.com>
2022-11-25 17:40:56 +0300

    some refactoring that's it.

--- main.py
+++ main.py
@@ -5,31 +5,34 @@ from UCS import UCS
 from UserPlay import UserPlay
 
 
-def prompt(board):
+def prompt(b):
     i = input("what would you like to do: \n1)- Play yourself\n2)- DFS\n3)- BFS\n4)- UCS\n5)- A*\n6)- Compare Algorithms\n")
     match i:
         case "1":
-            return UserPlay(board)
+            return UserPlay(b)
         case "2":
-            return DFS(board)
+            return DFS(b)
         case "3":
-            return BFS(board)
+            return BFS(b)
         case "4":
-            return UCS(board)
+            return UCS(b)
         # case "5":
         #    return Astar(board)
         case "6":
-            print(" UCS stats:", UCS(board).loop(), "\n", "DFS stats:", DFS(board).loop(), "\n", "BFS stats:",
-                  BFS(board).loop())
+            b1 = Board(b.Dimensions, b.KingPosition, b.CastlePosition, b.Walls)
+            b2 = Board(b.Dimensions, b.KingPosition, b.CastlePosition, b.Walls)
+            b3 = Board(b.Dimensions, b.KingPosition, b.CastlePosition, b.Walls)
+            print(" UCS stats:", UCS(b1).loop(), "\n", "DFS stats:", DFS(b2).loop(), "\n", "BFS stats:",
+                  BFS(b3).loop())
             exit()
         case _:
             exit()
 
 
 if __name__ == '__main__':
-    b = Board((4, 4), (2, 2), (4, 4), [(1, 1)], filename='', path=None)
+    board = Board((4, 4), (2, 2), (4, 4), [(1, 1)], filename='', path=None)
 
-    b.print()
+    board.print()
 
-    prompt(b).loop()
+    prompt(board).loop()
     exit()
